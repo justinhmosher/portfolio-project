9126abd082961d6bf860f0596e0aaae745960bad
unknown <justinhmosher@gmail.com>
2023-01-04 16:33:42 -0800

    Blog app successfully running

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
 	title = models.CharField(max_length = 200)
-	pub_date = models.DateField(auto_now = FALSE,auto_now_add = FALSE)
-	body = models.CharField(max_length = none)
-	image = models.ImageField(upload_to ='images/')
+	pub_date = models.DateField(auto_now = False,auto_now_add = False)
+	body = models.CharField(max_length = 30000)
+	blog_image = models.ImageField(upload_to ='blog-images/')
 # title
 # pub_date
 # body
