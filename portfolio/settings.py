8343073f0a4c5a8dc18ce5795492901b73af72f3
unknown <justinhmosher@gmail.com>
2023-01-05 16:39:34 -0800

    edited settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'#Bigpooey123',
+        'PASSWORD':'justin123',
         'HOST':'localhost',
         'PORT':'5432',
     }
